022c5e7bbdacc9c2c624392b782fc8d259da8213
Tlhalefang Ntshilane <Tlhalefangntshilane@gmail.com>
2025-10-24 00:40:29 +0100

    Refactor layout component and improve auth handling

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,48 +1,51 @@
-import { SplashScreen, Stack, useRouter, useSegments } from "expo-router";
+import { SplashScreen } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { useRouter, useSegments, Slot } from "expo-router";
 
 import { useAuthStore } from "../store/authStore";
-import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const router = useRouter();
   const segments = useSegments();
-
   const { checkAuth, user, token } = useAuthStore();
 
   const [fontsLoaded] = useFonts({
     "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
   });
 
+  // Hide splash screen when fonts are loaded
   useEffect(() => {
     if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded]);
 
+  // Check authentication on mount
   useEffect(() => {
     checkAuth();
   }, []);
 
-  // handle navigation based on the auth state
+  // Handle navigation based on auth state
   useEffect(() => {
     const inAuthScreen = segments[0] === "(auth)";
     const isSignedIn = user && token;
 
-    if (!isSignedIn && !inAuthScreen) router.replace("/(auth)");
-    else if (isSignedIn && inAuthScreen) router.replace("/(tabs)");
+    if (!isSignedIn && !inAuthScreen) {
+      router.replace("/(auth)");
+    } else if (isSignedIn && inAuthScreen) {
+      router.replace("/(tabs)");
+    }
   }, [user, token, segments]);
 
   return (
     <SafeAreaProvider>
       <SafeScreen>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="(auth)" />
-        </Stack>
+        {/* The Slot renders nested routes dynamically */}
+        <Slot />
       </SafeScreen>
       <StatusBar style="dark" />
     </SafeAreaProvider>
